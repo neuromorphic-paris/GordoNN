cfa0a5d61e18802ee3bf40389f35fadea1515d8f
marcorax <marcorax93@gmail.com>
2019-01-30 10:28:49 +0100

    Added new set of parameters to bench

--- Benchmark.py
+++ Benchmark.py
@@ -25,7 +25,7 @@ from Libs.Benchmark_Libs import  bench, refined_bench, compute_m_v
 os.environ['MKL_NUM_THREADS'] = '1'
 
 # Simultaneus threads you want to utilise on your machine 
-threads = 25
+threads = 50
 
 # Plotting settings
 sns.set()
@@ -61,7 +61,7 @@ labels = ("On","Off")
 dataset = Parallel(n_jobs=threads)(delayed(on_off_load)(number_files_dataset, train_test_ratio, shuffle_seed, use_all_addr) for run in range(runs))
 [_,_,labels_train, labels_test, filenames_train, labels_train]=np.transpose(dataset)
 #%% Load Networks parameter saved from the Playground
-file_name = parameter_folder+"GordoNN_Params_1L_40_L_2019-01-28 15:34:25.934600.pkl"
+file_name = parameter_folder+"GordoNN_Params_2L_8_2019-01-30 10:25:07.053386.pkl"
 with open(file_name, 'rb') as f:
     [basis_number, context_lengths, input_channels, taus_T, taus_2D] = pickle.load(f)   
 net_parameters = [basis_number, context_lengths, input_channels, taus_T, taus_2D]   
@@ -88,17 +88,17 @@ mean,var = compute_m_v(best_prediction_rates)
 #plt.show()
 #%% Save Results
 now=datetime.datetime.now()
-res_file_name='1L_40_L_'+str(now)+'.pkl'
+res_file_name='2L_8_'+str(now)+'.pkl'
 with open(result_folder+res_file_name, 'wb') as f:
     pickle.dump([file_name,mean,var,prediction_rates,best_prediction_rates], f)
 #%% Save Datasets
 now=datetime.datetime.now()
-res_file_name='Dataset_1L_40_L_'+str(now)+'.pkl'
+res_file_name='Dataset_2L_8_'+str(now)+'.pkl'
 with open(result_folder+res_file_name, 'wb') as f:
     pickle.dump([labels_train, labels_test, filenames_train, labels_train,
                  number_files_dataset,train_test_ratio], f)
 #%% Load Results
-#res_file_name='2019-01-25 22:12:37.441920.pkl'
+#res_file_name='1L_8_2019-01-28 20:42:16.929366.pkl'
 #result_folder = "Results/On_Off/"
 #with open(result_folder+res_file_name, 'rb') as f:
 #       results = pickle.load(f)
--- Playground.py
+++ Playground.py
@@ -93,8 +93,8 @@ legend = ("On","Off") # Legend containing the labes used for plots
 # =============================================================================
 
 
-basis_number = [[8,40]] 
-context_lengths = [8,30,40]
+basis_number = [[8,40],[10,20]] 
+context_lengths = [8,10,40]
 input_channels = 32 + 32*use_all_addr
 
 channel_taus = np.array([45, 56, 70, 88, 111, 139, 175, 219, 275, 344, 432, 542, 679, 851, 1067,
@@ -145,6 +145,6 @@ plt.show()
 #%% Save network parameters
 
 now=datetime.datetime.now()
-file_name = "GordoNN_Params_1L_40_L_"+str(now)+".pkl"
+file_name = "GordoNN_Params_2L_8_"+str(now)+".pkl"
 with open(parameter_folder+file_name, 'wb') as f:
     pickle.dump([basis_number, context_lengths, input_channels, taus_T, taus_2D], f)
